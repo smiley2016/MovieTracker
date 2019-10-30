53cfe37ed83cee197bd69bfd82e469220b52cc61
Csaba <smileyb666@gmail.com>
2019-10-30 14:11:10 +0200

    File Commented

--- app/src/main/java/com/smartsoft/movietracker/presenter/GenreSelectorPresenter.java
+++ app/src/main/java/com/smartsoft/movietracker/presenter/GenreSelectorPresenter.java
@@ -12,10 +12,29 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
+/**
+ * @see GenreSelectorPresenter using for the saving the data we get from the API
+ * and send these data to {@link com.smartsoft.movietracker.view.main.genre.GenreSelectorFragment}
+ */
+
 public class GenreSelectorPresenter {
 
+    /**
+     * @see #TAG is saves the class name.
+     */
     public static String TAG = GenreSelectorPresenter.class.getName();
 
+    /**
+     * @see #updateGenres(GenreSelectorInterface) function is calls the
+     * method of the {@link ApiController} namely getAllGenres() function
+     * to get the data from the server.
+     * When the data downloaded in the {@link Observer} onNext() function called the
+     * interface.updateGenres() function. Which is implemented in
+     * {@link com.smartsoft.movietracker.view.main.genre.GenreSelectorFragment}
+     * @param genreSelectorInterface used for to communicate between
+     *                               {@link com.smartsoft.movietracker.view.main.genre.GenreSelectorFragment}
+     *                               {@link GenreSelectorPresenter}
+     */
     public void updateGenres(GenreSelectorInterface genreSelectorInterface) {
         ApiController.getInstance().getAllGenres()
                 .subscribeOn(Schedulers.io())
@@ -46,8 +65,10 @@ public class GenreSelectorPresenter {
                 });
     }
 
-
-    //TODO: Ennek a MovieNavigationInterface-nak adj valami mas nevet mert elegge megteveszto (pl GenreView vagy valami hasonlo)
+    /**
+     * @see GenreSelectorInterface is used for presenter will have a point where
+     * can call the respected Fragment implementation to update the data on the userInterface.
+     */
     public interface GenreSelectorInterface {
         void updateGenres(ArrayList<Genre> list);
     }
