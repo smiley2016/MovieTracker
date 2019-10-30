a11804d6559d1b5e46210e57333c05c6595cb292
Csaba <smileyb666@gmail.com>
2019-10-30 11:14:34 +0200

    TextView size edited

--- app/build/generated/not_namespaced_r_class_sources/debug/r/butterknife/runtime/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/butterknife/runtime/R.java
@@ -36,28 +36,28 @@ public final class R {
     public static final class dimen {
         private dimen() {}
 
-        public static final int compat_button_inset_horizontal_material = 0x7f06006a;
-        public static final int compat_button_inset_vertical_material = 0x7f06006b;
-        public static final int compat_button_padding_horizontal_material = 0x7f06006c;
-        public static final int compat_button_padding_vertical_material = 0x7f06006d;
-        public static final int compat_control_corner_material = 0x7f06006e;
-        public static final int compat_notification_large_icon_max_height = 0x7f06006f;
-        public static final int compat_notification_large_icon_max_width = 0x7f060070;
-        public static final int notification_action_icon_size = 0x7f0601a6;
-        public static final int notification_action_text_size = 0x7f0601a7;
-        public static final int notification_big_circle_margin = 0x7f0601a8;
-        public static final int notification_content_margin_start = 0x7f0601a9;
-        public static final int notification_large_icon_height = 0x7f0601aa;
-        public static final int notification_large_icon_width = 0x7f0601ab;
-        public static final int notification_main_column_padding_top = 0x7f0601ac;
-        public static final int notification_media_narrow_margin = 0x7f0601ad;
-        public static final int notification_right_icon_size = 0x7f0601ae;
-        public static final int notification_right_side_padding_top = 0x7f0601af;
-        public static final int notification_small_icon_background_padding = 0x7f0601b0;
-        public static final int notification_small_icon_size_as_large = 0x7f0601b1;
-        public static final int notification_subtext_size = 0x7f0601b2;
-        public static final int notification_top_pad = 0x7f0601b3;
-        public static final int notification_top_pad_large_text = 0x7f0601b4;
+        public static final int compat_button_inset_horizontal_material = 0x7f06006b;
+        public static final int compat_button_inset_vertical_material = 0x7f06006c;
+        public static final int compat_button_padding_horizontal_material = 0x7f06006d;
+        public static final int compat_button_padding_vertical_material = 0x7f06006e;
+        public static final int compat_control_corner_material = 0x7f06006f;
+        public static final int compat_notification_large_icon_max_height = 0x7f060070;
+        public static final int compat_notification_large_icon_max_width = 0x7f060071;
+        public static final int notification_action_icon_size = 0x7f0601a7;
+        public static final int notification_action_text_size = 0x7f0601a8;
+        public static final int notification_big_circle_margin = 0x7f0601a9;
+        public static final int notification_content_margin_start = 0x7f0601aa;
+        public static final int notification_large_icon_height = 0x7f0601ab;
+        public static final int notification_large_icon_width = 0x7f0601ac;
+        public static final int notification_main_column_padding_top = 0x7f0601ad;
+        public static final int notification_media_narrow_margin = 0x7f0601ae;
+        public static final int notification_right_icon_size = 0x7f0601af;
+        public static final int notification_right_side_padding_top = 0x7f0601b0;
+        public static final int notification_small_icon_background_padding = 0x7f0601b1;
+        public static final int notification_small_icon_size_as_large = 0x7f0601b2;
+        public static final int notification_subtext_size = 0x7f0601b3;
+        public static final int notification_top_pad = 0x7f0601b4;
+        public static final int notification_top_pad_large_text = 0x7f0601b5;
     }
     public static final class drawable {
         private drawable() {}
@@ -125,18 +125,18 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0f007c;
+        public static final int status_bar_notification_info_overflow = 0x7f0f007b;
     }
     public static final class style {
         private style() {}
 
-        public static final int TextAppearance_Compat_Notification = 0x7f1000f9;
-        public static final int TextAppearance_Compat_Notification_Info = 0x7f1000fa;
-        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f1000fc;
-        public static final int TextAppearance_Compat_Notification_Time = 0x7f1000ff;
-        public static final int TextAppearance_Compat_Notification_Title = 0x7f100101;
-        public static final int Widget_Compat_NotificationActionContainer = 0x7f100190;
-        public static final int Widget_Compat_NotificationActionText = 0x7f100191;
+        public static final int TextAppearance_Compat_Notification = 0x7f100122;
+        public static final int TextAppearance_Compat_Notification_Info = 0x7f100123;
+        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f100125;
+        public static final int TextAppearance_Compat_Notification_Time = 0x7f100128;
+        public static final int TextAppearance_Compat_Notification_Title = 0x7f10012a;
+        public static final int Widget_Compat_NotificationActionContainer = 0x7f1001bc;
+        public static final int Widget_Compat_NotificationActionText = 0x7f1001bd;
     }
     public static final class styleable {
         private styleable() {}
