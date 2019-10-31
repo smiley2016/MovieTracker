18058ec87967db550eb528293adfa4139d434637
Csaba <smileyb666@gmail.com>
2019-10-31 13:52:18 +0200

    File Commented

--- app/src/main/java/com/smartsoft/movietracker/utils/StringUtils.java
+++ app/src/main/java/com/smartsoft/movietracker/utils/StringUtils.java
@@ -1,9 +1,13 @@
 package com.smartsoft.movietracker.utils;
 
+/**
+ * This class contains those strings
+ * what uses the app
+ */
 public class StringUtils {
     public static final String EMPTY_STRING = "";
     public static final String HYPHEN_DELIMITER = "-";
-    public static final String COMMA_DELIMITER = ",";
+    static final String COMMA_DELIMITER = ",";
     public static final String COMMA_DELIMITER_WITH_SPACE = ", ";
     public static final String HYPHEN_DELITMITER_WITH_SPACE_IN_FRONT_AND_BACK = " - ";
 }
