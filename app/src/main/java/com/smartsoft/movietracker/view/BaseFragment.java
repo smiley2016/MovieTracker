ef575a0ca21c90cb5d418dbbcc7b11d698e83feb
Csaba Bartalus <smartpath02>
2019-11-01 13:45:05 +0200

    File commented

--- app/src/main/java/com/smartsoft/movietracker/view/BaseFragment.java
+++ app/src/main/java/com/smartsoft/movietracker/view/BaseFragment.java
@@ -1,26 +1,40 @@
 package com.smartsoft.movietracker.view;
 
-import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.smartsoft.movietracker.utils.FragmentNavigation;
 
+/**
+ * This is the main fragment who is extended
+ * from the {@link Fragment} and contains the most based functions of the
+ * app that used in every Fragment
+ */
 public abstract class BaseFragment extends Fragment {
 
+    /**
+     * This contains the inflated View
+     */
     protected View rootView;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    /**
+     * This function dismiss the {@link com.smartsoft.movietracker.view.internet.NoInternetDialogFragment}
+     * when the Internet comes back but it has overrode in the extended
+     * fragments
+     */
     public void onInternetConnected() {
         FragmentNavigation.getInstance().dismissFragment();
     }
 
+    /**
+     * This function calls the {@link FragmentNavigation#removeNoInternetDialogFragmentFromBackStack()}
+     * because this checks if the {@link com.smartsoft.movietracker.view.internet.NoInternetDialogFragment}
+     * is still in the backStack or not, and finally create a new instance from the
+     * {@link com.smartsoft.movietracker.view.internet.NoInternetDialogFragment}
+     * if there is "no internet" and that's the most important because this function will be called
+     * in this situation.
+     */
     public void onInternetDisconnected() {
 
         FragmentNavigation.getInstance().removeNoInternetDialogFragmentFromBackStack();
