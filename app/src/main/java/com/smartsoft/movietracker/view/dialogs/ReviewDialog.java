eee0823218dc1a688521fa1e073fb548f267ba38
Csaba Bartalus <smartpath02>
2019-11-01 11:47:03 +0200

    File commented

--- app/src/main/java/com/smartsoft/movietracker/view/dialogs/ReviewDialog.java
+++ app/src/main/java/com/smartsoft/movietracker/view/dialogs/ReviewDialog.java
@@ -8,27 +8,68 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.smartsoft.movietracker.R;
+import com.smartsoft.movietracker.model.review.Review;
 import com.smartsoft.movietracker.utils.Utils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * This class holds the {@link com.smartsoft.movietracker.model.review.Review} elements
+ * and it's views on the {@link com.smartsoft.movietracker.view.detail.DetailPageFragment}
+ */
 public class ReviewDialog {
 
-    private static final String TAG = ReviewDialog.class.getName();
+    /**
+     * This view is used for the text what have to be scrollable
+     */
     @BindView(R.id.comment_scroll_view)
     ScrollView scroll;
+
+    /**
+     * {@link ReviewDialog} button which is used for dismiss the dialog
+     */
     @BindView(R.id.dialog_close_button)
     Button closeButton;
+
+    /**
+     * This {@link TextView} will be the {@link #comment}
+     */
     @BindView(R.id.dialog_review_comment)
     TextView reviewComment;
+
+    /**
+     * This {@link TextView} will be the {@link #author}
+     */
     @BindView(R.id.dialog_reviewer)
     TextView reviewer;
+
+    /**
+     * Review
+     */
     private String comment;
+    /**
+     * Who wrote the {@link #comment}
+     */
     private String author;
+
+    /**
+     * Where running the app
+     */
     private Context context;
 
 
+    /**
+     * CLass constructor
+     * Here sets the app the {@link #comment},
+     * {@link #author} and the {@link #context}.
+     * At the final the constructor calls the {@link #startReviewDialog()}
+     * function to will be appeared on the UI
+     *
+     * @param comment {@link Review#getContent()}
+     * @param author  Who wrote the {@link }
+     * @param context Where running the app
+     */
     public ReviewDialog(String comment, String author, Context context) {
         this.comment = comment;
         this.author = author;
@@ -36,12 +77,25 @@ public class ReviewDialog {
         startReviewDialog();
     }
 
-
+    /**
+     * Here makes the app the {@link ReviewDialog}
+     * from the {@link #context} and sets it's
+     * content view from an XML file
+     */
     private void startReviewDialog() {
         final Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.review_dialog);
 
         initViews(dialog);
+    }
+
+    /**
+     * Initializer function what sets views of the dialog to it's own
+     * properties
+     * @param dialog {@link ReviewDialog} what will be appeared on the UI
+     */
+    private void initViews(Dialog dialog) {
+        ButterKnife.bind(this, dialog);
 
         closeButton.setOnClickListener(view -> dialog.dismiss());
 
@@ -55,10 +109,6 @@ public class ReviewDialog {
         });
 
         dialog.show();
-    }
-
-    private void initViews(Dialog dialog) {
-        ButterKnife.bind(this, dialog);
     }
 
 }
