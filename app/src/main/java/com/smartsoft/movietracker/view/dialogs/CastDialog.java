6e86469c0cb889698a33c758745077e7f00711d3
Csaba Bartalus <smartpath02>
2019-11-01 11:33:51 +0200

    File commented

--- app/src/main/java/com/smartsoft/movietracker/view/dialogs/CastDialog.java
+++ app/src/main/java/com/smartsoft/movietracker/view/dialogs/CastDialog.java
@@ -13,23 +13,61 @@ import com.smartsoft.movietracker.utils.Constant;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * This dialog is used for hold the {@link Cast}
+ * member name and her/his picture
+ * on the {@link com.smartsoft.movietracker.view.detail.DetailPageFragment}
+ */
+
 public class CastDialog {
 
+    /**
+     * Actor/Actress picture
+     */
     @BindView(R.id.cast_dialog_image)
     ImageView poster;
+
+    /**
+     * Actor/actress name
+     */
     @BindView(R.id.cast_dialog_name)
     TextView name;
+
+    /**
+     * Close Button for to dismiss the {@link CastDialog}
+     */
     @BindView(R.id.cast_dialog_close)
     ImageView closeImageView;
+
+    /**
+     * Where running the app
+     */
     private Context context;
+
+    /**
+     * {@link Cast} member model
+     */
     private Cast cast;
 
+    /**
+     * Class constructor
+     * The app here sets the {@link #context}
+     * the {@link #cast}, and starts the {@link CastDialog}
+     *
+     * @param context Where running the app
+     * @param cast    {@link Cast} member
+     */
     public CastDialog(Context context, Cast cast) {
         this.context = context;
         this.cast = cast;
         startCastDialog();
     }
 
+    /**
+     * Here makes the app the {@link Dialog} from the {@link #context}
+     * and set it's content view from an XML file and calls the initializer function
+     * @see #initViews(Dialog)
+     */
     private void startCastDialog() {
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.cast_dialog);
@@ -38,6 +76,11 @@ public class CastDialog {
 
     }
 
+    /**
+     * This function set all the {@link CastDialog}'s views
+     * their properties and so on..
+     * @param dialog {@link Cast} dialog what will be appeared on the UI
+     */
     private void initViews(Dialog dialog) {
         ButterKnife.bind(this, dialog);
 
