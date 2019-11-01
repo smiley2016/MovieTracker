fb45bf43460e483c5c88cff465c74f9debf4837b
Csaba Bartalus <smartpath02>
2019-11-01 12:24:04 +0200

    File commented

--- app/src/main/java/com/smartsoft/movietracker/view/main/genre/GenreGridViewPresenter.java
+++ app/src/main/java/com/smartsoft/movietracker/view/main/genre/GenreGridViewPresenter.java
@@ -18,10 +18,26 @@ import com.smartsoft.movietracker.model.genre.Genre;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * This presenter handle the functionality of the
+ * {@link androidx.leanback.widget.VerticalGridView} in the
+ * {@link GenreSelectorFragment}
+ */
 public class GenreGridViewPresenter extends Presenter {
 
+    /**
+     * Where the {@link GenreGridViewPresenter} is running
+     */
     private Context mContext;
 
+    /**
+     * Overrode function from the abstract parent class.
+     * This handle the {@link R.layout#genre_element} will be
+     * a view from XML file.
+     *
+     * @param parent Wherein the view will be appeared
+     * @return {@link PresenterViewHolder} object
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent) {
         View view = LayoutInflater.from(parent.getContext())
@@ -30,17 +46,35 @@ public class GenreGridViewPresenter extends Presenter {
         return new PresenterViewHolder(view);
     }
 
+    /**
+     * Overrode from the abstract parent class
+     * This handle every single view which is inflated from
+     * the XML will be initialized, sets the views properties
+     * @param viewHolder {@link PresenterViewHolder} object
+     *                          uses the app to set the views properties
+     * @param item This item ({@link Genre} model) get the function from the
+     *      {@link androidx.leanback.widget.ArrayObjectAdapter}
+     */
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, Object item) {
         PresenterViewHolder holder = (PresenterViewHolder) viewHolder;
         holder.bind((Genre) item);
     }
 
+    /**
+     * The app doesn't use this function
+     * @param viewHolder {@link PresenterViewHolder}
+     */
     @Override
     public void onUnbindViewHolder(ViewHolder viewHolder) {
 
     }
 
+    /**
+     * Inner class for initialize the view what
+     * made from {@link R.layout#genre_element}
+     * and sets it's properties
+     */
     class PresenterViewHolder extends ViewHolder {
 
         @BindView(R.id.genre_name)
@@ -52,12 +86,20 @@ public class GenreGridViewPresenter extends Presenter {
         @BindView(R.id.selected_icon)
         ImageView select_icon;
 
+        /**
+         * Inner class constructor
+         * @param view inflated view
+         */
         PresenterViewHolder(View view) {
             super(view);
 
             ButterKnife.bind(this, view);
         }
 
+        /**
+         * Here sets the properties of the inflated view
+         * @param genre {@link Genre} model from the {@link androidx.leanback.widget.ArrayObjectAdapter}
+         */
         void bind(Genre genre) {
             genreNameTextView.setText(genre.getName());
 
